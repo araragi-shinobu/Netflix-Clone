d4833460d2140a02fd39451475b02dc3ec1f6da1
araragi-shinobu <785712896@qq.com>
2024-02-17 00:40:53 -0800

    update static setting

--- netflix_site/settings.py
+++ netflix_site/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'STATIC')
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
